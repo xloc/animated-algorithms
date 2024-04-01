57ca794038e6364158518c16cf012600f78ca9af
xloc <xloc.cc@outlook.com>
2024-03-31 18:43:53 -0700

    init commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="h-screen flex justify-center items-center">
+    <h1 class="text-4xl font-bold text-center">
+      Hello Vite + Vue 3 + TypeScript + Tailwind CSS!
+    </h1>
+  </div>
+</template>
